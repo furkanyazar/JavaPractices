0a5464899ccf4333471ffad7b95bd1eebf3a8cff
Furkan Yazar <furkanyazar9853@gmail.com>
2022-03-09 22:34:38 +0300

    FixtureBuilder | Update App.java

--- FixtureBuilder/src/App.java
+++ FixtureBuilder/src/App.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<String> list = new ArrayList<>();
+
+        list.add("Galatasaray");
+        list.add("Bursaspor");
+        list.add("Fenerbahçe");
+        list.add("Beşiktaş");
+        list.add("Başakşehir");
+        list.add("Trabzonspor");
+
+        FixtureGenerator fixtureGenerator = new FixtureGenerator();
+        List<List<Match>> all = fixtureGenerator.getFixtures(list);
+        
+        fixtureGenerator.print(all);
     }
+
 }
